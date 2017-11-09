959356ce1b1cc0278bc60511b69a84f1551d8750
Alex Custura <a.custura@student.vu.nl>
2017-11-09 21:36:51 +0100

    Implemented task 6b

--- ass1/task6.c
+++ ass1/task6.c
@@ -1,12 +1,19 @@
 #include<errno.h>
+#include<fcntl.h>
 #include<stdio.h>
 #include<sys/mman.h>
+#include<sys/stat.h>
 #include<sys/types.h>
+#include<unistd.h>
 
 int main(int argc, char* argv[])
 {
 	off_t buffer_size = 1UL << 40; // 1 TB
+	off_t phys_mem_offset = 1UL << 30; // 1GB
 	char *buffer = (char*)mmap(NULL, (size_t) buffer_size, PROT_READ | PROT_WRITE | PROT_EXEC, MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
+	int phys_mem_fd;
+	int i;
+	unsigned char c;
 	
 	if(buffer == MAP_FAILED)
 	{
@@ -20,6 +27,33 @@ int main(int argc, char* argv[])
 	{
 		perror("Failed to unmap memory.");
 		return -1;
+	}
+	
+	phys_mem_fd = open("/dev/mem", O_RDONLY);
+	
+	if(phys_mem_fd < 0)
+	{
+		perror("Failed to open /dev/mem");
+		return -1;
+	}
+	
+	if(lseek(phys_mem_fd, phys_mem_offset, SEEK_SET) < 0)
+	{
+		perror("Failed to seek in /dev/mem");
+		return -1;
+	}
+	
+	// print 10 bytes from physical memory at address >=1GB
+	for(i = 0; i < 10; i++)
+	{
+		if(read(phys_mem_fd, &c, sizeof(c)) < 0)
+		{
+			perror("Failed to read from /dev/mem");
+			close(phys_mem_fd);
+			return -1;
+		}
+		
+		printf("<0x%x>", c);
 	}
 	
 	return 0;
