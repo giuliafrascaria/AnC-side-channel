bbfa851b5265732e22d5034e3c8995deac787fd0
Alex Custura <a.custura@student.vu.nl>
2017-11-09 17:27:38 +0100

    First attempt at allocating 1TB of memory with mmap

---
+++ ass1/task6.c
@@ -0,0 +1,27 @@
+#include<errno.h>
+#include<stdio.h>
+#include<sys/mman.h>
+#include<sys/types.h>
+
+int main(int argc, char* argv[])
+{
+	off_t buffer_size = 1UL << 40; // 1 TB
+	char *buffer = (char*)mmap(NULL, (size_t) buffer_size, PROT_READ | PROT_WRITE | PROT_EXEC, MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
+	
+	if(buffer == MAP_FAILED)
+	{
+		perror("Failed to map memory.");
+		return -1;
+	}
+	
+	printf("Buffer size: %p\n", (void*) buffer);
+	
+	if(munmap(buffer, (size_t) buffer_size) < 0)
+	{
+		perror("Failed to unmap memory.");
+		return -1;
+	}
+	
+	return 0;
+}
+
