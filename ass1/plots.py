10f787f58355f354c1a050c5bc2d2a65c34b2dda
Alex Custura <a.custura@student.vu.nl>
2017-11-12 12:52:53 +0100

    Added script for plotting memory profile times

---
+++ ass1/plots.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+
+import os
+import matplotlib.pyplot as plt
+
+cached_filepath = "./cached.txt"
+uncached_filepath = "./uncached.txt"
+fig_filepath = "plots.png"
+
+def main():
+	cached = []
+	uncached = []
+	
+	if not os.path.exists(cached_filepath):
+		print("%s not found" % cached_filepath)
+		return
+		
+	if not os.path.exists(uncached_filepath):
+		print("%s not found" % uncached_filepath)
+		return
+		
+	cached = [int(line) for line in open(cached_filepath)]
+	uncached = [int(line) for line in open(uncached_filepath)]
+	
+	fig = plt.figure()
+	
+	plt.plot(cached, 'g')
+	plt.plot(uncached, 'r')
+	plt.ylabel('Clock ticks')
+	plt.xlabel('Experiment number')
+	
+	if os.path.exists(fig_filepath):
+		os.remove(fig_filepath)
+	
+	fig.savefig(fig_filepath)
+
+if __name__ == "__main__":
+	main()
+
