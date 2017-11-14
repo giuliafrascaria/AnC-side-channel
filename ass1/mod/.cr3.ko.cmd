c5e945682bd7fea62a907a783599232c3537d9ea
giuliafrascaria <giulia.frascaria@gmail.com>
2017-11-14 13:52:32 +0100

    added module binaries to gitignore, removed png from gitignore

--- ass1/mod/.cr3.ko.cmd
+++
@@ -1,1 +0,0 @@
-cmd_/home/lucian/secure_software/ass1/mod/cr3.ko := ld -r -m elf_x86_64 -T ./scripts/module-common.lds --build-id  -o /home/lucian/secure_software/ass1/mod/cr3.ko /home/lucian/secure_software/ass1/mod/cr3.o /home/lucian/secure_software/ass1/mod/cr3.mod.o ;  true
