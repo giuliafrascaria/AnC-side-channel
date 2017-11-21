05becb68e19491421606acc8b0be0b3914ec204c
giuliafrascaria <giulia.frascaria@gmail.com>
2017-11-21 15:37:03 +0100

    evict tlb at cacheline granularity

--- ass2/prog.c
+++ ass2/prog.c
@@ -65,28 +65,28 @@ int evict_itlb(volatile unsigned char *buffer, size_t size)
 {
 	int i, j, maxj;
 	fp ptr;
-	
+
 	if(buffer == NULL)
 	{
 		return -1;
 	}
-	
+
 	// execute eviction set instructions
 	for(i = 0; i < size; i += PAGE_SIZE)
 	{
-		maxj = i + PAGE_SIZE - 1;
-		
+		maxj = i + 63 ;
+
 		for(j = i; j < maxj; j++)
 		{
-			buffer[j] = 0x90;	
+			buffer[j] = 0x90;
 		}
-		
+
 		buffer[maxj] = 0xc3;
 		ptr = (fp)(&(buffer[i]));
-		
+
 		ptr();
 	}
-	
+
 	return 0;
 }
 
@@ -94,15 +94,15 @@ volatile unsigned long* get_pointer_to_pte(Page page)
 {
 	volatile unsigned long* ret;
 	int fd = open("/dev/mem", O_RDONLY);
-	
+
 	if(fd < 0)
 	{
 		perror("Failed to open /dev/mem");
 		return NULL;
 	}
-	
+
 	ret = (unsigned long*) mmap(NULL, PAGE_SIZE, PROT_READ|PROT_WRITE, MAP_PRIVATE, fd, page.address);
-	
+
 	return ret;
 }
 
@@ -121,7 +121,7 @@ void profile_mem_access(volatile unsigned char* c, volatile unsigned long* pte, 
 		perror("Failed to open file for printing memory access profiles");
 		return;
 	}
-	
+
 	if(touch == 1)
 	{
 		c[0] = 0x90; //nop
@@ -184,18 +184,18 @@ Page* get_phys_addr(volatile unsigned char *buffer, off_t buffer_size)
 	unsigned long frame_offset_mask = 0xfff; // last 12 bits
 	Page* physical_addresses;
 	int fd;
-	
+
 	if(buffer == NULL)
 	{
 		printf("Cannot get physical address of null pointer\n");
 		return NULL;
 	}
-	
+
 	buffer_address = (unsigned long) &buffer;
-	
+
 	// array containing 4 physical addresses of PT pages and the translated VA
 	physical_addresses = (Page*)malloc(5 * sizeof(Page));
-	
+
 	if(physical_addresses == NULL)
 	{
 		perror("Failed to allocate memory for storing physical addresses");
@@ -203,14 +203,14 @@ Page* get_phys_addr(volatile unsigned char *buffer, off_t buffer_size)
 	}
 
 	fd = open("/dev/mem", O_RDONLY);
-	
+
 	if(fd < 0)
 	{
 		perror("Failed to open /dev/mem");
 		free(physical_addresses);
 		return NULL;
 	}
-	
+
 	if((physical_addresses[0].address = get_page_table_root()) < 0)
 	{
 		return NULL;
@@ -240,7 +240,7 @@ Page* get_phys_addr(volatile unsigned char *buffer, off_t buffer_size)
 
 	physical_addresses[4].offset = buffer_address & frame_offset_mask;
 	printf("buffer physical address: 0x%lx\n", physical_addresses[4].address | physical_addresses[4].offset);
-	
+
 	return physical_addresses;
 }
 
@@ -255,24 +255,24 @@ int main(int argc, char* argv[])
 	volatile unsigned long* page_ptr;
 	volatile unsigned long* pte;
 	volatile unsigned char *target = (unsigned char*)mmap(NULL, target_size, PROT_READ | PROT_WRITE | PROT_EXEC, MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
-	
+
 	if(target == MAP_FAILED)
 	{
 		perror("Failed to map memory.");
 		return -1;
 	}
-	
+
 	cache_flush_set = (int*)malloc(cache_flush_set_size);
-	
+
 	if(cache_flush_set == NULL)
 	{
 		perror("Failed to allocate memory for cache flush set");
 		munmap((void*) target, target_size);
 		return -1;
 	}
-	
+
 	ev_set = (unsigned char*)mmap(NULL, ev_set_size, PROT_READ | PROT_WRITE | PROT_EXEC, MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
-	
+
 	if(ev_set == NULL)
 	{
 		perror("Failed to initialize TLB eviction set");
@@ -280,9 +280,9 @@ int main(int argc, char* argv[])
 		free(cache_flush_set);
 		return -1;
 	}
-	
+
 	pages = get_phys_addr(target, target_size);
-	
+
 	if(pages == NULL)
 	{
 		printf("Failed to get physical address of target VA\n");
@@ -290,9 +290,9 @@ int main(int argc, char* argv[])
 		munmap((void*) ev_set, ev_set_size);
 		free(cache_flush_set);
 	}
-	
+
 	page_ptr = get_pointer_to_pte(pages[2]); // using PTL2 to trigger side effect
-	
+
 	if(page_ptr == NULL)
 	{
 		printf("Failed to map target page table page\n");
@@ -301,12 +301,12 @@ int main(int argc, char* argv[])
 		free(cache_flush_set);
 		free(pages);
 	}
-	
+
 	pte = page_ptr + pages[2].offset;
 
 	profile_mem_access(target, pte, cache_flush_set, cache_flush_set_size, ev_set, ev_set_size, 0, "uncached.txt");
 	profile_mem_access(target, pte, cache_flush_set, cache_flush_set_size, ev_set, ev_set_size, 1, "hopefully_cached.txt");
-	
+
 	free((void*)pte);
 	munmap((void*)page_ptr, PAGE_SIZE);
 	munmap((void*) target, target_size);
