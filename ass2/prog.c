313d79d0957aeb8a0474b53f8b93f137059694b3
Alex Custura <a.custura@student.vu.nl>
2017-11-21 14:57:32 +0100

    Measuring access to page table entry, as discussed today with Ben

--- ass2/prog.c
+++ ass2/prog.c
@@ -13,6 +13,12 @@
 
 typedef void (*fp)(void);
 
+typedef struct
+{
+	unsigned long address;
+	unsigned long offset;
+} Page;
+
 // returns the CR3 register value (physical address of PGD)
 unsigned long get_page_table_root()
 {
@@ -84,7 +90,23 @@ int evict_itlb(volatile unsigned char *buffer, size_t size)
 	return 0;
 }
 
-void profile_mem_access(volatile unsigned char* c, int* buffer, size_t cache_flush_set_size, volatile unsigned char* ev_set, size_t ev_set_size, int touch, char* filename)
+volatile unsigned long* get_pointer_to_pte(Page page)
+{
+	volatile unsigned long* ret;
+	int fd = open("/dev/mem", O_RDONLY);
+	
+	if(fd < 0)
+	{
+		perror("Failed to open /dev/mem");
+		return NULL;
+	}
+	
+	ret = (unsigned long*) mmap(NULL, PAGE_SIZE, PROT_READ|PROT_WRITE, MAP_PRIVATE, fd, page.address);
+	
+	return ret;
+}
+
+void profile_mem_access(volatile unsigned char* c, volatile unsigned long* pte, int* buffer, size_t cache_flush_set_size, volatile unsigned char* ev_set, size_t ev_set_size, int touch, char* filename)
 {
 	int i, j, k;
 	unsigned long long hi1, lo1;
@@ -132,7 +154,7 @@ void profile_mem_access(volatile unsigned char* c, int* buffer, size_t cache_flu
 							"mov %%rdx, %0\n\t"
 							"mov %%rax, %1\n\t" : "=r"(hi1), "=r"(lo1) : : "%rax", "%rbx", "%rcx", "%rdx");
 
-		asm volatile("movq (%0), %%rax\n" : : "c"(c) : "rax");
+		asm volatile("movq (%0), %%rax\n" : : "c"(pte) : "rax");
 
 		asm volatile ("RDTSCP\n\t"
 							"mov %%rdx, %0\n\t"
@@ -155,48 +177,71 @@ void profile_mem_access(volatile unsigned char* c, int* buffer, size_t cache_flu
 	fclose(f);
 }
 
-unsigned long get_phys_addr(volatile unsigned char *buffer, off_t buffer_size)
+Page* get_phys_addr(volatile unsigned char *buffer, off_t buffer_size)
 {
-	unsigned long buffer_address = (unsigned long) &buffer;
-	unsigned long page_table_offset;
+	unsigned long buffer_address;
 	unsigned long page_table_offset_mask = 0x1ff; // last 9 bits
 	unsigned long frame_offset_mask = 0xfff; // last 12 bits
-	unsigned long aux_phys_addr;
+	Page* physical_addresses;
 	int fd;
-
-	if((aux_phys_addr = get_page_table_root()) < 0)
+	
+	if(buffer == NULL)
 	{
-		return -1;
+		printf("Cannot get physical address of null pointer\n");
+		return NULL;
+	}
+	
+	buffer_address = (unsigned long) &buffer;
+	
+	// array containing 4 physical addresses of PT pages and the translated VA
+	physical_addresses = (Page*)malloc(5 * sizeof(Page));
+	
+	if(physical_addresses == NULL)
+	{
+		perror("Failed to allocate memory for storing physical addresses");
+		return NULL;
 	}
 
 	fd = open("/dev/mem", O_RDONLY);
+	
+	if(fd < 0)
+	{
+		perror("Failed to open /dev/mem");
+		free(physical_addresses);
+		return NULL;
+	}
+	
+	if((physical_addresses[0].address = get_page_table_root()) < 0)
+	{
+		return NULL;
+	}
 
 	printf("buffer virtual address: 0x%lx\n", buffer_address);
 
 	// level 4
-	printf("PT4 physical address: 0x%lx\n", aux_phys_addr);
-	page_table_offset = (buffer_address >> 39) & page_table_offset_mask;
-	aux_phys_addr = get_physical_addr(fd, aux_phys_addr, page_table_offset);
+	printf("PT4 physical address: 0x%lx\n", physical_addresses[0].address);
+	physical_addresses[0].offset = (buffer_address >> 39) & page_table_offset_mask;
+	physical_addresses[1].address = get_physical_addr(fd, physical_addresses[0].address, physical_addresses[0].offset);
 
 	// level 3
-	printf("PT3 physical address: 0x%lx\n", aux_phys_addr);
-	page_table_offset = (buffer_address >> 30) & page_table_offset_mask;
-	aux_phys_addr = get_physical_addr(fd, aux_phys_addr, page_table_offset);
+	printf("PT3 physical address: 0x%lx\n", physical_addresses[1].address);
+	physical_addresses[1].offset = (buffer_address >> 30) & page_table_offset_mask;
+	physical_addresses[2].address = get_physical_addr(fd, physical_addresses[1].address, physical_addresses[1].offset);
 
 	// level 2
-	printf("PT2 physical address: 0x%lx\n", aux_phys_addr);
-	page_table_offset = (buffer_address >> 21) & page_table_offset_mask;
-	aux_phys_addr = get_physical_addr(fd, aux_phys_addr, page_table_offset);
+	printf("PT2 physical address: 0x%lx\n", physical_addresses[2].address);
+	physical_addresses[2].offset = (buffer_address >> 21) & page_table_offset_mask;
+	physical_addresses[3].address = get_physical_addr(fd, physical_addresses[2].address, physical_addresses[2].offset);
 
 	// level 1
-	printf("PT1 physical address: 0x%lx\n", aux_phys_addr);
-	page_table_offset = (buffer_address >> 12) & page_table_offset_mask;
-	aux_phys_addr = get_physical_addr(fd, aux_phys_addr, page_table_offset);
+	printf("PT1 physical address: 0x%lx\n", physical_addresses[3].address);
+	physical_addresses[3].offset = (buffer_address >> 12) & page_table_offset_mask;
+	physical_addresses[4].address = get_physical_addr(fd, physical_addresses[3].address, physical_addresses[3].offset);
 
-	aux_phys_addr = aux_phys_addr | (buffer_address & frame_offset_mask);
-	printf("buffer physical address: 0x%lx\n", aux_phys_addr);
+	physical_addresses[4].offset = buffer_address & frame_offset_mask;
+	printf("buffer physical address: 0x%lx\n", physical_addresses[4].address | physical_addresses[4].offset);
 	
-	return aux_phys_addr;
+	return physical_addresses;
 }
 
 int main(int argc, char* argv[])
@@ -205,8 +250,11 @@ int main(int argc, char* argv[])
 	size_t cache_flush_set_size = 10 * 1024 * 1024; // 10MB
 	size_t ev_set_size = 8192 * PAGE_SIZE; // 1024 TLB entries
 	int * cache_flush_set;
-	volatile unsigned char *target = (unsigned char*)mmap(NULL, target_size, PROT_READ | PROT_WRITE | PROT_EXEC, MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
 	volatile unsigned char *ev_set;
+	Page* pages;
+	volatile unsigned long* page_ptr;
+	volatile unsigned long* pte;
+	volatile unsigned char *target = (unsigned char*)mmap(NULL, target_size, PROT_READ | PROT_WRITE | PROT_EXEC, MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
 	
 	if(target == MAP_FAILED)
 	{
@@ -233,14 +281,38 @@ int main(int argc, char* argv[])
 		return -1;
 	}
 	
-	//get_phys_addr(target, target_size);
-
-	profile_mem_access(target, cache_flush_set, cache_flush_set_size, ev_set, ev_set_size, 0, "uncached.txt");
-	profile_mem_access(target, cache_flush_set, cache_flush_set_size, ev_set, ev_set_size, 1, "hopefully_cached.txt");
+	pages = get_phys_addr(target, target_size);
 	
+	if(pages == NULL)
+	{
+		printf("Failed to get physical address of target VA\n");
+		munmap((void*) target, target_size);
+		munmap((void*) ev_set, ev_set_size);
+		free(cache_flush_set);
+	}
+	
+	page_ptr = get_pointer_to_pte(pages[2]); // using PTL2 to trigger side effect
+	
+	if(page_ptr == NULL)
+	{
+		printf("Failed to map target page table page\n");
+		munmap((void*) target, target_size);
+		munmap((void*) ev_set, ev_set_size);
+		free(cache_flush_set);
+		free(pages);
+	}
+	
+	pte = page_ptr + pages[2].offset;
+
+	profile_mem_access(target, pte, cache_flush_set, cache_flush_set_size, ev_set, ev_set_size, 0, "uncached.txt");
+	profile_mem_access(target, pte, cache_flush_set, cache_flush_set_size, ev_set, ev_set_size, 1, "hopefully_cached.txt");
+	
+	free((void*)pte);
+	munmap((void*)page_ptr, PAGE_SIZE);
 	munmap((void*) target, target_size);
 	munmap((void*) ev_set, ev_set_size);
 	free(cache_flush_set);
+	free(pages);
 
 	return 0;
 }
