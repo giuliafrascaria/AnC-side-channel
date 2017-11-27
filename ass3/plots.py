8f6af2d6a2a1b2a387e4538f0698f219b655825c
Alex Custura <a.custura@student.vu.nl>
2017-11-27 13:34:47 +0100

    Cleaned up plots.py

--- ass3/plots.py
+++ ass3/plots.py
@@ -4,49 +4,18 @@ import os
 import matplotlib.pyplot as plt
 import numpy as np
 
-cached_filepath = "./uncached.txt"
-uncached_filepath = "./uncached.txt"
 scan_filepath = "./scan.txt"
-fig_filepath = "plots.png"
 heat_filepath = "heatmap.png"
 
 def main():
-	cached = []
-	uncached = []
 
 	if not os.path.exists(cached_filepath):
 		print("%s not found" % cached_filepath)
 		return
-
-	#if not os.path.exists(uncached_filepath):
-	#	print("%s not found" % uncached_filepath)
-	#	return
-
-	cached_values = [int(line) for line in open(cached_filepath)]
+	
 	scan_values = [int(line) for line in open(scan_filepath)]
-	#uncached_values = [int(line) for line in open(uncached_filepath)]
 
 	fig = plt.figure()
-
-	# max_timing = max(cached_values)
-	# hist_bar_width = int(0.008 * max_timing)
-	# bins = [i for i in range(max_timing + hist_bar_width) if i % hist_bar_width == 0]
-
-
-
-	# plt.hist(cached_values, color='blue', bins=200)
-	#
-	#
-	# fig.text(0.5, 0.01, 'Clock ticks', ha='center')
-	# fig.text(0.03, 0.5, 'Experiment number', va='center', rotation='vertical')
-	#
-	# if os.path.exists(fig_filepath):
-	# 	os.remove(fig_filepath)
-	#
-	# fig.savefig(fig_filepath)
-
-
-	#a = np.random.random((8, 64))
 	a = np.reshape(scan_values, (-1, 64))
 	plt.imshow(a, cmap='hot', interpolation='nearest')
 	plt.show()
