ae71b1f6b67fa607c01ccb7ad5360fa7a37f9f2b
Alex Custura <a.custura@student.vu.nl>
2017-12-04 19:14:35 +0100

    Fixes to prog for signal at every level

--- ass5/prog.c
+++ ass5/prog.c
@@ -84,11 +84,9 @@ int evict_cacheline(volatile unsigned char *buffer, unsigned short int cache_lin
 	
 	cache_line_offset *= CACHE_LINE_SIZE; // convert offset to number of  bytes
 	
-	if(flush_lvl == FLUSH_ALL_PTL || flush_lvl == FLUSH_PTL1)
-	{
-		// flush L3 cache, unified TLB and translation cache for PTL1
-		evict_data(buffer, cache_line_offset, UNIFIED_TLB_SIZE, PAGE_SIZE_PTL1);
-	}
+	// flush L3 cache, unified TLB and translation cache for PTL1
+	evict_data(buffer, cache_line_offset, UNIFIED_TLB_SIZE, PAGE_SIZE_PTL1);
+	
 	
 	if(flush_lvl == FLUSH_ALL_PTL || flush_lvl == FLUSH_PTL2)
 	{
@@ -215,24 +213,24 @@ void scan_target(volatile unsigned char* c, volatile unsigned char* ev_set)
 		profile_mem_access(c, ev_set, 8 * i * (PAGE_SIZE_PTL4 + 2 * PAGE_SIZE_PTL3 + 3 * PAGE_SIZE_PTL2 + 4 * PAGE_SIZE_PTL1), FLUSH_ALL_PTL, "scan.txt");
 	}
 	
-	for(i = 0; i < 24; i++)
+	for(i = 0; i < 8; i++)
 	{
-		profile_mem_access(c, ev_set, i * PAGE_SIZE_PTL1, FLUSH_ALL_PTL, "scan_1.txt");
+		profile_mem_access(c, ev_set, i * PAGE_SIZE_PTL1, FLUSH_PTL1, "scan_1.txt");
 	}
 	
-	for(i = 0; i < 24; i++)
+	for(i = 0; i < 8; i++)
 	{
-		profile_mem_access(c, ev_set, i * PAGE_SIZE_PTL2, FLUSH_ALL_PTL, "scan_2.txt");
+		profile_mem_access(c, ev_set, i * PAGE_SIZE_PTL2, FLUSH_PTL2, "scan_2.txt");
 	}
 	
-	for(i = 0; i < 24; i++)
+	for(i = 0; i < 8; i++)
 	{
-		profile_mem_access(c, ev_set, i * PAGE_SIZE_PTL3, FLUSH_ALL_PTL, "scan_3.txt");
+		profile_mem_access(c, ev_set, i * PAGE_SIZE_PTL3, FLUSH_PTL3, "scan_3.txt");
 	}
 	
-	for(i = 0; i < 24; i++)
+	for(i = 0; i < 8; i++)
 	{
-		profile_mem_access(c, ev_set, i * PAGE_SIZE_PTL4, FLUSH_ALL_PTL, "scan_4.txt");
+		profile_mem_access(c, ev_set, i * PAGE_SIZE_PTL4, FLUSH_PTL4, "scan_4.txt");
 	}
 }
 
